baf8f90d749c88749250623aa2011bef00d96de7
Ruoyu Fu <lingoerer@gmail.com>
2016-04-26 18:33:30 +0800

    Adding lint tool and fixing style issue

--- lib/runes.ex
+++ lib/runes.ex
@@ -1,17 +1,17 @@
 defmodule Freer.Runes do
 
-  defp lift?({:pure, _x} = freer), do: freer
-  defp lift?({:impure, _u, _c} = freer), do: freer 
-  defp lift?(g), do: Freer.eta(g)
+  defp lift({:pure, _x} = freer), do: freer
+  defp lift({:impure, _u, _c} = freer), do: freer 
+  defp lift(g), do: Freer.eta(g)
 
   def return(x), do: Freer.return(x)
 
   def bind(freer, f) do
-    etaf = &(lift?(f.(&1)))
-    lift?(freer) |> Freer.bind(etaf)
+    etaf = &(&1 |> f.() |> lift)
+    freer |> lift |> Freer.bind(etaf)
   end
 
-  def apply(freer_f, freer), do: lift?(freer_f) |> Freer.apply(lift?(freer)) 
+  def apply(freer_f, freer), do: Freer.apply(lift(freer_f), lift(freer)) 
 
-  def map(freer, f), do: lift?(freer) |> Freer.map(f) 
+  def map(freer, f), do: freer |> lift |> Freer.map(f) 
 end
--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Freer.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package,
      deps: deps]
   end
 
@@ -17,6 +18,12 @@ defmodule Freer.Mixfile do
     [applications: [:logger]]
   end
 
+  defp package do
+    [files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+     maintainers: ["Ruoyu Fu"],
+     licenses: ["Apache-2.0"],
+     links: %{"Github" => "https://github.com/lingoer/freer"}]
+  end
   # Dependencies can be Hex packages:
   #
   #   {:mydep, "~> 0.3.0"}
@@ -27,6 +34,9 @@ defmodule Freer.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{ :mix_ctags, github: "rgrinberg/mix_ctags" }]
+    [
+      { :mix_ctags, github: "rgrinberg/mix_ctags" },
+      { :credo, "~> 0.3"}
+    ]
   end
 end
